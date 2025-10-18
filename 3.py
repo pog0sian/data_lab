7a9ffb9a899f6d53198dcd4de3c6a6698a814acb
pog0sian <pogosian.david2018@yandex.ru>
2025-10-18 22:43:03 +0700

    Commit

--- 3.py
+++
@@ -1,9 +0,0 @@
-
-def recursive_poly(word):
-    if len(word) <= 1:
-        return "YES"
-    if word[0] != word[-1]:
-        return "NO"
-    return recursive_poly(word[1:-1])
-
-print(recursive_poly("racecar"))
